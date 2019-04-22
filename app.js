35ec75d725205b718656c4d0d29bf24c49a0b7c5
Guruprasad Rao <kshkrao3@gmail.com>
2019-04-22 16:50:02 +0530

    - Added config to ESv-6 - GraphQL types

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const events = [];
 
 app.use(express.json());
 
@@ -7,6 +11,52 @@ app.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
 
-app.get('/', (req, res, next) => {
-    res.send('Hello world');
+app.use('/api', graphqlHttp({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput{
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery{
+            events: [Event!]!
+        }
+
+        type RootMutation{
+            createEvent(eventInput: EventInput): Event
+        }
+        schema{
+            query: RootQuery
+            mutation: RootMutation
+        }
+        `),
+    rootValue: {
+        events: () => {
+            return events;
+        },
+        createEvent: (args) => {
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            };
+            events.push(event);
+            return event;
+        }
+    },
+    graphiql: true
+}), (req, res, next) => {
+
 });
