97bec3e3dab4ea6a0263fefdbdabd4f37eaf84c3
Guruprasad Rao <kshkrao3@gmail.com>
2019-04-22 18:28:46 +0530

    Connection with mongodb and storing and reading actual data from Database

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
-
-const events = [];
-
+const mongoose = require('mongoose');
+const Event = require('./models/event');
 app.use(express.json());
 
 app.listen(3000, () => {
@@ -42,21 +41,41 @@ app.use('/api', graphqlHttp({
         `),
     rootValue: {
         events: () => {
-            return events;
+            return Event.find((err, events) => {
+                if (err) throw err;
+                return events.map(event => {
+                    return Object.assign({}, event._doc);
+                });
+            });
         },
-        createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+        createEvent: args => {
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: args.eventInput.date
-            };
-            events.push(event);
-            return event;
+                date: new Date(args.eventInput.date)
+            });
+            return event.save()
+                .then(result => {
+                    console.log('Saved event successfully', result);
+                    return Object.assign({}, result._doc);
+                })
+                .catch(err => {
+                    console.log('Error while saving event', err);
+                    throw err;
+                });
         }
     },
     graphiql: true
 }), (req, res, next) => {
 
 });
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphql-event-dnara.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, { useNewUrlParser: true })
+    .then(() => {
+        console.log('Mongo Cluster connected');
+    }).catch(err => {
+        console.log('User', process.env.MONGO_USER);
+        console.log('Password', process.env.MONGO_PASSWORD);
+        console.log('Error while connecting mongo db', err);
+    });
